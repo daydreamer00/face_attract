33d2dabd5dd590ff9a7f979b58b8af8949b5ac3b
Daydreamer <boxi0418@gmail.com>
2014-05-05 13:34:59 +0800

    Change lambdasearch range

--- lambdasearch.m
+++ lambdasearch.m
@@ -5,12 +5,14 @@ function [  ] = lambdasearch(featurevector,quadraticflag)
     
     bestcv = Inf;
     
-    for log2lambda = 1:10,
-        [train_error_rate, test_error_rate, cv_error_rate]=Run(featurevector,2,quadraticflag,'bad.txt',2^log2lambda);
+    for log2lambda = -8:8,
+        fprintf('log2lambda=%g\n',log2lambda);
+        [train_error_rate, test_error_rate, cv_error_rate]=Run(featurevector,2,quadraticflag,'bad.txt',log2lambda);
         if (cv_error_rate < bestcv),
                 bestcv = cv_error_rate;
                 bestlog2lambda = log2lambda;
         end
+        
     end
     fprintf('best log2lambda=%g, err_rate=%g)\n', bestlog2lambda, bestcv);
 end
